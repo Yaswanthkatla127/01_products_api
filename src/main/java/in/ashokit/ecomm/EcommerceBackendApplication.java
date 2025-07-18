aeadf4a02a4ec6130681d8c594fd4d0455462038
Yaswanth Katla <141010596+Yaswanthkatla127@users.noreply.github.com>
2025-07-18 17:55:09 +0530

    Update EcommerceBackendApplication.java

--- src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -9,6 +9,7 @@ public class EcommerceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 		int j = 10;
+		int k = 15;
 		
 	}
 
